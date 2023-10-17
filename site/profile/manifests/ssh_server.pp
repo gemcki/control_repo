3a655ef6a0ac6d424023aecdc941062aa06413fb
gemcki <46629300+gemcki@users.noreply.github.com>
2023-10-16 20:38:53 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1+vhVEvKszCH2HZe9H2NJbMyt2g5dPIcY0jl7lsZ6N8EGkYIQH8bUgNEvEPpPFfwrd28OmudoW3kfg6eqUDTFwvX5ZBe876s8vr29zBS2GLILrmes8m+mB6+4KFS8guTMt7qD1ajGHm3k/wNsaYYqaCyfhdkvhMsopdq+LQEBbMSrUryOlYh0iT4XLt8ohhILXCJHj6TIUqyHDLS8Pi+TZchNyJvBmadlnd7OY4JzRi/K7yJ99aNbsE4C4C294hvzaoKxjtv3lklS9ZrveVFm15tXVxtE0d613woRGklMVRdlvlxWnrHcEMIRdje0otZ7xKvqKZScvi/h3k1Xs6k5',
 	}  
 }
