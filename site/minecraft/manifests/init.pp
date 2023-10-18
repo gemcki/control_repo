88bfd8cc83413146c18bf93052f3d7329cf96321
gemcki <46629300+gemcki@users.noreply.github.com>
2023-10-17 21:40:50 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
-  $url = 'https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar'
-  $install_dir = '/opt/minecraft'
+class minecraft (
+  $url = 'https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar',
+  $install_dir = '/opt/minecraft',
+){
   file {$install_dir:
     ensure => directory,
   }
